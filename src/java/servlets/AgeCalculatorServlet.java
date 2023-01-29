afa3bef5aadec8d33aa2b1b89344b19de8e52aa4
howard2022 <howard.hyca@gmail.com>
2023-01-29 11:10:39 -0700

    AgeCalculator run properly

--- src/java/servlets/AgeCalculatorServlet.java
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -15,18 +15,29 @@ public class AgeCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
                 .forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String age = request.getParameter("age");
+        String age1 = request.getParameter("age");
+        request.setAttribute("age", age1);
         
-        request.setAttribute("age", age);
+        if (age1.equals("") || age1==null){
+            request.setAttribute("message", "You must give your current age");
+        }
+        else if (!age1.matches("[0-9]+$")){
+            request.setAttribute("message", "You must enter only numbers");   
+        }
+        else {
+            int age = Integer.parseInt(age1);
+            age++;
+            request.setAttribute("message", "Your age next birthday will be " + age);
+        }
         
-        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
                 .forward(request, response);
     }
 
---
+++ web/WEB-INF/arithmeticCalculator.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : arithmeticCalculator
+    Created on : 28-Jan-2023, 10:46:15 PM
+    Author     : howar
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>hello</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
